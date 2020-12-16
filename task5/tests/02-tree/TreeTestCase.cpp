696c567cbe9090df4df7501c8dd22c369e1070ec
p-mazhnik <pavel.mazhnik@gmail.com>
2020-12-16 10:59:09 +0300

    setup temp files

--- task5/tests/02-tree/TreeTestCase.cpp
+++ task5/tests/02-tree/TreeTestCase.cpp
@@ -5,13 +5,36 @@
 
 #include "TreeTestCase.h"
 #include "Tree.h"
+#include <boost/filesystem.hpp>
+#include <fstream>
 
-TEST(TreeTestCase, all) {
+using boost::filesystem::create_directories;
+using boost::filesystem::remove_all;
+using boost::filesystem::current_path;
+
+void TreeTestCase::SetUp() {
+    boost::filesystem::path dir("./temp/inner");
+    if (boost::filesystem::create_directories(dir)) {
+        std::cout << "Setup temp directories successfully" << "\n";
+    }
+    std::ofstream file("./temp/TempFile.txt");
+    file.close();
+}
+
+void TreeTestCase::TearDown() {
+    boost::filesystem::path dir("./temp");
+    boost::filesystem::remove_all(dir);
+}
+
+TEST_F(TreeTestCase, exeptions) {
     ASSERT_THROW(GetTree("", false), std::invalid_argument);
-    ASSERT_THROW(GetTree("./Makefile", false), std::invalid_argument);
-    FileNode src_dirs = GetTree("./src", true);
+    ASSERT_THROW(GetTree("./temp/TempFile.txt", false), std::invalid_argument);
+}
+
+TEST_F(TreeTestCase, FileNode) {
+    FileNode src_dirs = GetTree("./temp", true);
     ASSERT_TRUE(src_dirs.is_dir);
-    ASSERT_EQ(src_dirs.name, "src");
-    ASSERT_EQ(src_dirs.children[0].name, GetTree("./src/01-simple-library", true).name);
-    ASSERT_EQ(src_dirs.children[0], GetTree("./src/01-simple-library", true));
+    ASSERT_EQ(src_dirs.name, "temp");
+    ASSERT_EQ(src_dirs.children[0].name, GetTree("./temp/inner", true).name);
+    ASSERT_EQ(src_dirs.children[0], GetTree("./temp/inner", true));
 }
--- task5/tests/02-tree/TreeTestCase.h
+++ task5/tests/02-tree/TreeTestCase.h
@@ -9,6 +9,9 @@
 #include <boost/filesystem/path.hpp>
 
 class TreeTestCase : public ::testing::Test {
+public:
+    void SetUp() override;
+    void TearDown() override;
 };
 
 
